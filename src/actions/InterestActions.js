a232e0a962f5118d8aa61fbaf3d74946bf7defd3
Chris Philbin <chris@chrisphilbin.net>
2021-05-07 11:47:11 -0400

    Stubbing out basic actions

---
+++ src/actions/InterestActions.js
@@ -0,0 +1,7 @@
+export const GET_INTERESTS         = "GET_INTERESTS"
+export const GET_INTERESTS_SUCCESS = "GET_INTERESTS_SUCCESS"
+export const GET_INTERESTS_FAILURE = "GET_INTERESTS_FAILURE"
+
+export const getInterests = () => (
+    { type: GET_INTERESTS }
+)
