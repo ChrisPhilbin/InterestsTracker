1186aad78c74e6928dae5e1d5004e9d9a8a69763
Chris Philbin <chris@chrisphilbin.net>
2021-05-08 07:07:44 -0400

    Adding dayjs to calculate how many days ago an employee was last interacted with

--- src/components/employees/EmployeeCard.js
+++ src/components/employees/EmployeeCard.js
@@ -2,11 +2,16 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
+import dayjs from 'dayjs'
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 
+
 const useStyles = makeStyles((theme) => ({
+    overdue: {
+        color: 'red'
+    },
     root: {
         minWidth: 470
     }
@@ -18,6 +23,7 @@ const EmployeeCard = (props) => {
     const classes = useStyles()
 
     let employee = props.employee
+    let lastInteraction = dayjs(props.employee.lastInteraction).fromNow()
 
     return(
         <Grid item xs={12} sm={6} key={employee.id}>
@@ -25,6 +31,9 @@ const EmployeeCard = (props) => {
                 <CardContent>
                     <Typography variant="h5" component="h2">
                         {employee.name}
+                    </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                        Last interaction: {lastInteraction >= 4 ? <span className="classes.overdue">{lastInteraction}</span> : {lastInteraction} }
                     </Typography>
                 </CardContent>
             </Card>
