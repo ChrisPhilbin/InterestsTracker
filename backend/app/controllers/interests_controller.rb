a886c681dc0e087ce648f4fc3826e362fd2086c6
Chris Philbin <chris@chrisphilbin.net>
2021-05-09 19:44:59 -0400

    Adding actions to interests controller

--- backend/app/controllers/interests_controller.rb
+++ backend/app/controllers/interests_controller.rb
@@ -1,8 +1,28 @@
 class InterestsController < ApplicationController
 
+    #GET /employees/:employee_id/interests
     def index
         @interests = Interest.where(employee_id: params[:employee_id])
         render :json => @interests
+    end
+
+    #POST /employees/:employee_id/interests
+    def create
+        @interest = Interest.create!(interest_params)
+        render :json => @interest
+    end
+
+    #GET /employees/:employee_id/interests/:interest_id
+    def show
+        @employee = Employee.find(params[:employee_id])
+        @interests = @employee.interests
+        render :json => @interests
+    end
+
+    private
+
+    def interest_params
+        params.permit(:name)
     end
 
 end
