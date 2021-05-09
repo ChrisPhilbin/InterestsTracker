e131b2a5474aa98eaab55d81c3b6f4bac75e8202
Chris Philbin <chris@chrisphilbin.net>
2021-05-09 19:03:07 -0400

    Updating has_many relationship and defining index action

--- backend/app/controllers/interests_controller.rb
+++ backend/app/controllers/interests_controller.rb
@@ -1,2 +1,8 @@
 class InterestsController < ApplicationController
+
+    def index
+        @interests = Interest.where(employee_id: params[:employee_id])
+        render :json => @interests
+    end
+
 end
