0fa21531f264c1ca08437b2bd0781698e4b709ff
Chris Philbin <chris@chrisphilbin.net>
2021-05-16 09:55:01 -0400

    Adding action for newsfeed in employee controller

--- backend/app/controllers/employees_controller.rb
+++ backend/app/controllers/employees_controller.rb
@@ -34,6 +34,13 @@ class EmployeesController < ApplicationController
         render :json => @employee
     end
 
+    #GET /employees/:employee_id/newsfeed
+    def newsfeed 
+        @employee = Employee.find(:params[:id])
+        @news = Employee.update_all_employee_interests(@employee.interests, @employee.pets, @employee.sports)
+        render :json => @news
+    end
+
 
     private
 
