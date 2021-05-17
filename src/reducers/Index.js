31b662c5fd1004572e436456bb9610f8ff29a4c0
Chris Philbin <chris@chrisphilbin.net>
2021-05-16 20:08:15 -0400

    Creating reducer for newsfeeds

--- src/reducers/Index.js
+++ src/reducers/Index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import employeesReducer from './EmployeesReducer'
 import interestsReducer from './InterestsReducer'
+import newsfeedReducer from './NewsfeedsReducer'
 import petsReducer from './PetsReducer'
 import sportsReducer from './SportsReducer'
 import notesReducer from './NotesReducer'
@@ -8,6 +9,7 @@ import notesReducer from './NotesReducer'
 const rootReducer = combineReducers({
     employees: employeesReducer,
     interests: interestsReducer,
+    newsfeeds: newsfeedReducer,
     pets:      petsReducer,
     sports:    sportsReducer,
     notes:     notesReducer
---
+++ src/reducers/NewsfeedsReducer.js
@@ -0,0 +1,21 @@
+import * as actions from '../actions/NewsfeedActions'
+
+export const initialNewsfeedState = {
+    news: [],
+    loading: false,
+    hasErrors: false
+}
+
+export default function newsfeedReducer(state = initialNewsfeedState, action) {
+    switch(action.type) {
+        case actions.GET_NEWSFEED:
+            return { ...state, loading: true }
+        case actions.GET_NEWSFEED_SUCCESS:
+            return { ...state, loading: false, news: action.payload }
+        case actions.GET_NEWSFEED_FAILURE:
+            return { ...state, loading: false, hasErrors: true }
+        default:
+            return state
+    }
+}
+
