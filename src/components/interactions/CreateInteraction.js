dcfbc8f50a2451b23983ddf1a7c006b8dd44e6c5
Chris Philbin <chris@chrisphilbin.net>
2021-05-22 07:56:26 -0400

    Creating component to allow creation of interaction events

---
+++ src/components/interactions/CreateInteraction.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateInteraction = (props) => {
+    return(
+        <>
+            Log an interaction
+        </>
+    )
+}
+
+export default CreateInteraction
