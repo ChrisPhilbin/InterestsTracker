8eaf4fe52bf3fda13a0d4187fa714935cc304cfe
Chris Philbin <chris@chrisphilbin.net>
2021-05-24 07:42:23 -0400

    Creating action and action creators for posting a new action

--- src/actions/InteractionActions.js
+++ src/actions/InteractionActions.js
@@ -2,6 +2,9 @@ export const GET_INTERACTIONS         = "GET_INTERACTIONS"
 export const GET_INTERACTIONS_SUCCESS = "GET_INTERACTIONS_SUCCESS"
 export const GET_INTERACTIONS_FAILURE = "GET_INTERACTIONS_FAILURE"
 
+export const INTERACTION_CREATED_SUCCESS = "INTERACTION_CREATED_SUCCESS"
+export const INTERACTION_CREATED_FAILURE = "INTERACTION_CREATED_FAILURE"
+
 export const getInteractions = () => (
     { type: GET_INTERACTIONS }
 )
@@ -23,5 +26,30 @@ export const fetchEmployeeInteractions = (employee_id) => {
         } catch (error) {
             dispatch(getInteractionsFailure(error))
         }
+    }
+}
+
+export const interactionCreatedSuccess = (interaction) => (
+    { type: INTERACTION_CREATED_SUCCESS, payload: interaction }
+)
+
+export const interactionCreatedFailure = (error) => (
+    { type: INTERACTION_CREATED_FAILURE, payload: error }
+)
+
+export const fetchPostNewInteraction = (interaction) => {
+    return async (dispatch) => {
+        try {
+            const response = await fetch(`http://localhost:3001/employees/${interaction.employee_id}/interactions`, {
+                method: 'POST',
+                body: JSON.stringify(interaction),
+                headers: { 'Content-Type':'application/json'}
+            })
+            const newInteraction = await response.json()
+            dispatch(interactionCreatedSuccess(newInteraction))
+        } catch (error) {
+            dispatch(interactionCreatedFailure(error))
+        }
+
     }
 }
