f7c9af8fc1b4241357b4ec3f1771462a77e2f5d9
Chris Philbin <chris@chrisphilbin.net>
2021-05-29 08:40:45 -0400

    Updating reducer to filter by id instead of interaction_id

--- src/reducers/InteractionsReducer.js
+++ src/reducers/InteractionsReducer.js
@@ -15,7 +15,7 @@ export default function interactionsReducer(state =initialInteractionsState, act
         case actions.GET_INTERACTIONS_FAILURE:
             return { ...state, loading: false, hasErrors: true }
         case actions.INTERACTION_DELETED_SUCCESS:
-            return { interactions: state.interactions.filter(interaction => interaction.interaction_id !== action.payload.interaction_id) }
+            return { interactions: state.interactions.filter(interaction => interaction.id !== action.payload.id) }
         default:
             return state
     }
