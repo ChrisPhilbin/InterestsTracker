b6f507f9b2e77f2923c74b708560d63f6cc04a0f
Chris Philbin <chris@chrisphilbin.net>
2021-06-04 19:18:22 -0400

    Working on adding devise jwt for authentication

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users,
+  controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+  }
+  get '/member-data', to: 'members#show'
+  
   get '/employees/:employee_id/newsfeed', to: 'employees#newsfeed'
   get '/employees/dashboard',             to: 'employees#dashboard'
 
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_113439) do
+ActiveRecord::Schema.define(version: 2021_06_04_131236) do
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 2021_05_22_113439) do
     t.index ["employee_id"], name: "index_interests_on_employee_id"
   end
 
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
+
   create_table "notes", force: :cascade do |t|
     t.text "contents"
     t.integer "employee_id", null: false
@@ -65,6 +71,16 @@ ActiveRecord::Schema.define(version: 2021_05_22_113439) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["employee_id"], name: "index_sports_on_employee_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "interactions", "employees"
