083c485ea59c15cd965c6ef6e743816131c1600a
Chris Philbin <chris@chrisphilbin.net>
2021-06-11 17:08:15 -0400

    Addressing code formatting

--- src/navigation/TopNav.js
+++ src/navigation/TopNav.js
@@ -1,39 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Badge from '@material-ui/core/Badge'
-import NotificationsIcon from '@material-ui/icons/Notifications'
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade'
-import { Link } from 'react-router-dom'
-import { fetchEmployeeAlerts } from '../actions/AlertActions';
-import useIsLoggedIn from '../hooks/useIsLoggedIn'
-import defaultFetchOptions from '../config/FetchDefault'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Badge from "@material-ui/core/Badge";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import { Link } from "react-router-dom";
+import { fetchEmployeeAlerts } from "../actions/AlertActions";
+import useIsLoggedIn from "../hooks/useIsLoggedIn";
+import defaultFetchOptions from "../config/FetchDefault";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
@@ -45,48 +45,52 @@ const useStyles = makeStyles((theme) => ({
 export default function TopNav() {
   const classes = useStyles();
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const isLoggedIn = useIsLoggedIn()
+  const isLoggedIn = useIsLoggedIn();
 
   useEffect(() => {
-    dispatch(fetchEmployeeAlerts)
-  }, [dispatch])
+    dispatch(fetchEmployeeAlerts);
+  }, [dispatch]);
 
   const handleLogout = () => {
-    console.log("handling logging out!")
-    fetch('http://localhost:3001/users/sign_out', {
+    console.log("handling logging out!");
+    fetch("http://localhost:3001/users/sign_out", {
       ...defaultFetchOptions,
-      method: 'DELETE'
-    })
-    localStorage.removeItem("AuthToken")
-    window.location.replace("/")
-  }
+      method: "DELETE",
+    });
+    localStorage.removeItem("AuthToken");
+    window.location.replace("/");
+  };
 
-  let alerts = useSelector(state => state.alerts.alerts)
+  let alerts = useSelector((state) => state.alerts.alerts);
 
-  let [open, setOpen] = useState(false)
+  let [open, setOpen] = useState(false);
 
-  let alertIcon
+  let alertIcon;
 
-  const [anchorEl, setAnchorEl] = React.useState(null)
-  const openMenu = Boolean(anchorEl)
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const openMenu = Boolean(anchorEl);
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget)
-  }
+    setAnchorEl(event.currentTarget);
+  };
   const handleClose = () => {
-    setAnchorEl(null)
-  }
+    setAnchorEl(null);
+  };
 
   if (Object.keys(alerts).length) {
     alertIcon = (
-      <Badge badgeContent={alerts.overdue_alerts.length} color="secondary" onClick={() => setOpen(true)}>
+      <Badge
+        badgeContent={alerts.overdue_alerts.length}
+        color="secondary"
+        onClick={() => setOpen(true)}
+      >
         <NotificationsIcon />
       </Badge>
-    )
+    );
   }
 
-  let modal
+  let modal;
 
   if (Object.keys(alerts).length) {
     modal = (
@@ -105,44 +109,69 @@ export default function TopNav() {
         <Fade in={open}>
           <div className={classes.paper}>
             <h2 id="transition-modal-title">Active alerts</h2>
-            <p id="transition-modal-description"><strong>Try to interact with the following employees:</strong></p>
+            <p id="transition-modal-description">
+              <strong>Try to interact with the following employees:</strong>
+            </p>
             {alerts.overdue_alerts.map((employee) => (
-              <span key={employee.id}><Link to={`/employees/${employee.id}`} onClick={() => setOpen(false)}>{employee.name}</Link><br /></span>
+              <span key={employee.id}>
+                <Link
+                  to={`/employees/${employee.id}`}
+                  onClick={() => setOpen(false)}
+                >
+                  {employee.name}
+                </Link>
+                <br />
+              </span>
             ))}
-            <p id="transition-modal-description"><strong>Today's birthdays:</strong></p>
-            {alerts.todays_birthdays.length ?
+            <p id="transition-modal-description">
+              <strong>Today's birthdays:</strong>
+            </p>
+            {alerts.todays_birthdays.length ? (
               <>
                 {alerts.todays_birthdays.map((employee) => (
                   <span>{employee.name}</span>
                 ))}
               </>
-            :
+            ) : (
               <span>No birthdays today</span>
-            }
-            <p id="transition-modal-description"><strong>Upcoming birthdays:</strong></p>
-            {alerts.upcoming_birthdays.length ?
+            )}
+            <p id="transition-modal-description">
+              <strong>Upcoming birthdays:</strong>
+            </p>
+            {alerts.upcoming_birthdays.length ? (
               <>
                 {alerts.upcoming_birthdays.map((employee) => (
-                  <span key={employee.id}><Link to={`/employees/${employee.id}`} onClick={() => setOpen(false)}>{employee.name}</Link></span>
+                  <span key={employee.id}>
+                    <Link
+                      to={`/employees/${employee.id}`}
+                      onClick={() => setOpen(false)}
+                    >
+                      {employee.name}
+                    </Link>
+                  </span>
                 ))}
               </>
-            :
+            ) : (
               <span>No upcoming birthdays</span>
-            }
+            )}
           </div>
         </Fade>
       </Modal>
-    )
+    );
   }
 
   return (
     <div className={classes.root}>
-
       {modal}
 
-      <AppBar position="static" style={{ background: '#005151' }}>
+      <AppBar position="static" style={{ background: "#005151" }}>
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
             <MenuIcon onClick={handleClick} />
 
             <Menu
@@ -151,19 +180,48 @@ export default function TopNav() {
               open={openMenu}
               onClose={handleClose}
               MenuListProps={{
-                'aria-labelledby': 'basic-button',
+                "aria-labelledby": "basic-button",
               }}
             >
-              <MenuItem component={Link} to="/" onClick={handleClose}>Home</MenuItem>
-              <MenuItem component={Link} to="/employees" onClick={handleClose}>My team</MenuItem>
-              <MenuItem component={Link} to="/" onClick={handleLogout}>Logout</MenuItem>
+              <MenuItem component={Link} to="/" onClick={handleClose}>
+                Home
+              </MenuItem>
+              <MenuItem component={Link} to="/employees" onClick={handleClose}>
+                My team
+              </MenuItem>
+              <MenuItem component={Link} to="/" onClick={handleLogout}>
+                Logout
+              </MenuItem>
             </Menu>
           </IconButton>
-          <Typography variant="h6" className={classes.title} component={Link} to="/employees" style={{ color: 'white', textDecoration: 'none' }}>
+          <Typography
+            variant="h6"
+            className={classes.title}
+            component={Link}
+            to="/employees"
+            style={{ color: "white", textDecoration: "none" }}
+          >
             Realationships
           </Typography>
-            {alertIcon}
-            { isLoggedIn ? <Button color="inherit" style={{ paddingLeft: 20 }} onClick={() => handleLogout()}>Logout</Button> : <Button color="inherit" component={Link} to="/sign_in" style={{ paddingLeft: 20 }}>Login</Button>}
+          {alertIcon}
+          {isLoggedIn ? (
+            <Button
+              color="inherit"
+              style={{ paddingLeft: 20 }}
+              onClick={() => handleLogout()}
+            >
+              Logout
+            </Button>
+          ) : (
+            <Button
+              color="inherit"
+              component={Link}
+              to="/sign_in"
+              style={{ paddingLeft: 20 }}
+            >
+              Login
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </div>
