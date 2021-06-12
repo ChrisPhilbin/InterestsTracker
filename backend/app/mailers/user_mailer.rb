478cf7f91fb62e6c6fdd8ec786d2ff195d2cb4ec
Chris Philbin <chris@chrisphilbin.net>
2021-06-12 07:20:31 -0400

    Defining daily_recommendation email

--- backend/app/mailers/user_mailer.rb
+++ backend/app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+
+    def daily_recommendations(user)
+        @user = User.find(user.id)
+        mail(to: @user.email, subject: "Your team's recommendations")
+    end
+    
 end
